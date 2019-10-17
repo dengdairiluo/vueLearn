e76aaf0364a39db9596e1ce21884731d924520b8
dengdairiluo <1020447548@qq.com>
2019-10-17 17:46:25 +0800

    vue学习20191017

--- src/assets/js/1.js
+++ src/assets/js/1.js
@@ -29,3 +29,38 @@ var app4 = new Vue({
         ]
     }
 })
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: '蔬菜' },
+            { id: 1, text: '奶酪' },
+            { id: 2, text: '随便其他什么人吃的东西'}
+        ]
+    }
+})
